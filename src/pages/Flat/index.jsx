d61225ff3700c9a343c573d424016a63b97ea88b
Julien Roche <merlin441977@gmail.com>
2023-09-03 21:42:59 +0200

    mise à jour style pages Flat

--- src/pages/Flat/index.jsx
+++ src/pages/Flat/index.jsx
@@ -11,32 +11,34 @@ function FicheLogement() {
     const { logementId } = useParams()
     for (let logement of logements) {
         if (logementId === logement.id) {
-            return (<div>
-                <Carroussel listePhotos={logement.pictures}/>
-                
+            return (<div className="flat-main">
+                <Carroussel listePhotos={logement.pictures} />
+
                 <div className="logement-item">
                     <div className="logement-ident">
-                        <div className='logement-name'>                       
-                        <h2 className="logement-title">{logement.title}</h2>
-                        <h3 className="logement-subtitle">{logement.location}</h3>
+                        <div className='logement-name'>
+                            <h2 className="logement-title">{logement.title}</h2>
+                            <h3 className="logement-subtitle">{logement.location}</h3>
                         </div>
-                        
+
                         <div className='logement-grid-tags'>
-                        <ul className="logement-tags">{logement.tags.map((tag) =>
-                            <li key={tag} className="tag">{tag}</li>)}
-                        </ul>
+                            <ul className="logement-tags">{logement.tags.map((tag) =>
+                                <li key={tag} className="tag">{tag}</li>)}
+                            </ul>
                         </div>
-    
+
                     </div>
 
                     <div className="logement-host">
                         <div className="host-ident">
-                            <h3 className="host-name">{logement.host.name}</h3>
+                            <h3 className="host-name">a mettre sur 2 lignes avec map et si on lit
+                            un" " espace vide, on met une blaise br{logement.host.name}</h3>
                             <img className="host-picture" src={logement.host.picture} alt={logement.host.name} />
 
                         </div>
-                        <Rating className="logement-rating" stars={logement.rating}/>
-
+                        <div className="logement-rating">
+                            <Rating stars={logement.rating} />
+                        </div>
                     </div>
 
 
